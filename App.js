e00ab1323090d547718c5a02b66f3549c8e65255
Javiera-Leiva <73081737+Javiera-Leiva@users.noreply.github.com>
2023-07-08 11:31:04 -0600

    App.js
    Creación del App.js en la que se codifica la navegación con todas las vistas y aplicación de estilos.

--- App.js
+++ App.js
@@ -1,12 +1,64 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import InicioScreen from './src/InicioScreen';
+import DetallesScreen from './src/DetallesScreen';
+import SeguimientoScreen from './src/SeguimientoScreen';
 
-const App = () => {
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Inicio"
+          component={InicioScreen}
+          options={{
+            title: 'Inicio',
+            headerStyle: {
+              backgroundColor: '#FFCD85',
+            },
+            headerTitleStyle: {
+              fontSize: 24,
+              fontWeight: 'bold',
+              color: '#000000',
+            },
+          }}
+        />
+        <Stack.Screen
+          name="Detalles"
+          component={DetallesScreen}
+          options={{
+            title: 'Detalles',
+            headerStyle: {
+              backgroundColor: '#FFCD85',
+            },
+            headerTitleStyle: {
+              fontSize: 24,
+              fontWeight: 'bold',
+              color: '#000000',
+            },
+          }}
+        />
+        <Stack.Screen
+          name="Seguimiento"
+          component={SeguimientoScreen}
+          options={{
+            title: 'Seguimiento',
+            headerStyle: {
+              backgroundColor: '#FFCD85',
+            },
+            headerTitleStyle: {
+              fontSize: 24,
+              fontWeight: 'bold',
+              color: '#000000',
+            },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
-export default App
+export default App;
