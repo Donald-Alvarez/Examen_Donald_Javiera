bf7870d2044a5c563a7b9761f1b0ec3a29d2af1f
Donald-Alvarez <donaldoctavio0210@gmail.com>
2023-07-08 21:53:02 -0600

    Aregacion de comentarios en el app.js

--- App.js
+++ App.js
@@ -1,34 +1,36 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import InicioScreen from './src/InicioScreen';
-import DetallesScreen from './src/DetallesScreen';
-import SeguimientoScreen from './src/SeguimientoScreen';
+import * as React from 'react';// Importa la biblioteca React y todas sus funciones y componentes
+import { NavigationContainer } from '@react-navigation/native';// Importa el contenedor de navegación de React Navigation
+import { createNativeStackNavigator } from '@react-navigation/native-stack';// Importa el tipo de navegación de stack nativo de React Navigation
+import InicioScreen from './src/InicioScreen';// Importa el componente InicioScreen desde el archivo './src/InicioScreen'
+import DetallesScreen from './src/DetallesScreen'; // Importa el componente DetallesScreen desde el archivo './src/DetallesScreen'
+import SeguimientoScreen from './src/SeguimientoScreen';// Importa el componente SeguimientoScreen desde el archivo './src/SeguimientoScreen'
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator();// Crea un objeto de tipo Stack utilizando la función createNativeStackNavigator de React Navigation
 
-function App() {
+function App() {// Declara el componente principal de la aplicación llamado App
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
+    /* Contenedor de navegación principal que envuelve toda la aplicación */
+    <NavigationContainer> 
+      <Stack.Navigator> 
         <Stack.Screen
-          name="Inicio"
-          component={InicioScreen}
+          name="Inicio" // Nombre de la primera pantalla
+          component={InicioScreen}// Componente asociado a la primera pantalla (InicioScreen)
           options={{
-            title: 'Inicio',
+            /* Opciones de configuración de la pantalla */
+            title: 'Inicio',// Título de la pantalla
             headerStyle: {
-              backgroundColor: '#FFCD85',
+              backgroundColor: '#FFCD85',//Color de la pantalla
             },
             headerTitleStyle: {
-              fontSize: 24,
-              fontWeight: 'bold',
-              color: '#000000',
+              fontSize: 24,  // Tamaño de fuente del título
+              fontWeight: 'bold', // Peso de la fuente del título
+              color: '#000000', // Color del título
             },
           }}
         />
         <Stack.Screen
-          name="Detalles"
-          component={DetallesScreen}
+          name="Detalles" // Nombre de la segunda pantalla
+          component={DetallesScreen}  // Componente asociado a la segunda pantalla (DetallesScreen)
           options={{
             title: 'Detalles',
             headerStyle: {
@@ -42,8 +44,8 @@ function App() {
           }}
         />
         <Stack.Screen
-          name="Seguimiento"
-          component={SeguimientoScreen}
+          name="Seguimiento" // Nombre de la tercera pantalla pantalla
+          component={SeguimientoScreen} // Componente asociado a la tercera pantalla (SeguimientoScreen)
           options={{
             title: 'Seguimiento',
             headerStyle: {
